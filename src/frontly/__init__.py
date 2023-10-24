4f4ffb915440fdde991fc306d4825b9994027ec3
Ed Menendez <ed@digitalhaiku.com>
2023-10-23 19:26:44 -0600

    Fix issues with packaging of static files and templates.

--- src/frontly/__init__.py
+++ src/frontly/__init__.py
@@ -2,7 +2,7 @@
 """
 from django.conf import settings
 
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 
 FRONTLY_CHECK_KILLSWITCH_PERM = getattr(settings, 'FRONTLY_CHECK_KILLSWITCH_PERM', True)
 FRONTLY_SANITIZE = getattr(settings, 'FRONTLY_SANITIZE', True)
